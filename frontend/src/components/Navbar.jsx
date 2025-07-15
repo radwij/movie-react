97cb610a712157f4e565073fe6686915b2d1de78
radwij <118460863+radwij@users.noreply.github.com>
2025-07-15 13:31:54 +0700

    feat: add navbar buttons animation

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,21 +8,21 @@ function Navbar() {
         <div className="nav-left flex gap-x-5 items-center">
           <img src={companyLogo} alt="logo" className="h-8 flex-shrink-0"/>
           <div className="nav-links space-x-4">
-            <Link to="/" className="text-base font-semibold text-black">Home</Link>
-            <Link to="/discussion" className="text-base font-semibold text-black">Discussion</Link>
+            <Link to="/" className="text-base font-semibold text-black hover:text-blue-500 transition-colors duration-300">Home</Link>
+            <Link to="/discussion" className="text-base font-semibold text-black hover:text-blue-500 transition-colors duration-300">Discussion</Link>
           </div>
         </div>
         <form className="flex search-form gap-x-3 whitespace-nowrap flex-grow h-fit">
           <input
             type="text"
             placeholder="Look up something..."
-            className="search-input w-full border-2 border-gray-200 px-3 py-2.5 rounded-lg placeholder:text-center focus:outline-0 focus:border-blue-500 focus:border-3"
+            className="search-input w-full border-2 border-gray-200 px-5 py-2 rounded-3xl focus:outline-0 focus:border-blue-500 focus:border-3"
           />
-          <button type="submit" className="py-2.5 px-4 bg-blue-600 rounded-lg">
+          <button type="submit" className="py-2 px-4 bg-blue-500 rounded-lg not-last-of-type:hover:bg-blue-700 transition-colors duration-300">
             <i className="fa-solid fa-magnifying-glass text-white"></i>
           </button>
         </form>
-        <Link to="" className="text-base font-semibold text-white px-5 py-2.5 bg-blue-600 rounded-lg w-fit whitespace-nowrap">Join Us</Link>
+        <Link to="" className="text-base font-semibold text-white px-5 py-2 bg-blue-500 rounded-lg w-fit whitespace-nowrap hover:bg-blue-600 transition-colors duration-300">Join Us</Link>
       </div>
     </nav>
   )
